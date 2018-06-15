4f77ddacea89af7a5f6b892d20e46d84732b15cd
Marcin Konrad Ceglarek <marcin.ceglarek@gmail.com>
2018-06-15 23:38:06 +0200

    Added some files

---
+++ ExcelDataGenerator/ExcelDataGenerator/ExcelDataGenerator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExcelDataGenerator
+{
+    public class ExcelDataGenerator
+    {
+        static Random rnd = new Random();
+
+        private ExcelDataGeneratorOptions options;
+
+        public ExcelDataGenerator(ExcelDataGeneratorOptions options)
+        {
+            this.options = options;
+        }
+
+        public List<Order> GenerateOrders()
+        {
+            var orderId = 1000 + rnd.Next(1000);
+            var retVal = new List<Order>();
+            for (var currentDate = this.options.StartDate; currentDate <= this.options.EndDate; currentDate = currentDate.AddDays(1))
+            {
+                retVal.Add(this.GenerateOrder(orderId++, currentDate));
+                retVal.Add(this.GenerateOrder(orderId++, currentDate));
+                retVal.Add(this.GenerateOrder(orderId++, currentDate));
+            }
+
+            return retVal;
+        }
+
+        private Order GenerateOrder(int id, DateTime date)
+        {
+            var customer = this.options.Customers[rnd.Next(this.options.Customers.Count)];
+            var salesPerson = this.options.SalesPeople[rnd.Next(this.options.SalesPeople.Count)];
+            var product = this.options.Products[rnd.Next(this.options.Products.Count)];
+            var productCount = rnd.Next(20);
+
+            var retVal = new Order()
+            {
+                OrderId = id,
+                OrderDate = date,
+                CustomerId = customer.Id,
+                CustomerName = customer.Name,
+                CustomerAddress = customer.Address,
+                CustomerCity = customer.City,
+                CustomerPostCode = customer.PostCode,
+                CustomerCountry = customer.Country,
+                SalesPersonName = salesPerson.Name,
+                SalesPersonRegion = salesPerson.Region,
+                ProductName = product.Name,
+                ProductCategory = product.Category,
+                ProductQuantity = productCount,
+                ProductUnitPrice = product.UnitPrice,
+                ShippingCost = (decimal)Math.Sqrt(productCount) * product.ShippingPrice
+            };
+
+            return retVal;
+        }
+    }
+
+    public class ExcelDataGeneratorOptions
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public List<Customer> Customers { get; set; }
+
+        public List<SalesPerson> SalesPeople { get; set; }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal ShippingPrice { get; set; }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class SalesPerson
+    {
+        public string Name { get; set; }
+        public string Region { get; set; }
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int Quarter => ((int)((this.OrderDate.Month - 1) / 3)) + 1;
+        public string Month => this.OrderDate.ToString("MMMM");
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerCity { get; set; }
+        public string CustomerPostCode { get; set; }
+        public string CustomerCountry { get; set; }
+        public string SalesPersonName { get; set; }
+        public string SalesPersonRegion { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public decimal ProductUnitPrice { get; set; }
+        public int ProductQuantity { get; set; }
+        public decimal ProductTotalPrice => this.ProductUnitPrice * this.ProductQuantity;
+        public decimal ShippingCost { get; set; }
+    }
+}
---
+++ ExcelDataGenerator/ExcelDataGenerator/Runner.cs
@@ -0,0 +1,25 @@
+﻿namespace ExcelDataGenerator
+{
+    public class Runner
+    {
+        public void Run()
+        {
+            var options = new ExcelDataGeneratorOptions()
+            {
+                StartDate = new System.DateTime(2017, 01, 01),
+                EndDate = new System.DateTime(2017, 12, 31),
+                Customers = new System.Collections.Generic.List<Customer>()
+                {
+                    new Customer() { Id = 1, Name = "Klient A", Address = "Adres A", City = "Miasto A", Country = "Polska", PostCode = "00-000"},
+                    new Customer() { Id = 2, Name = "Klient B", Address = "Adres B", City = "Miasto B", Country = "Polska", PostCode = "00-000"},
+                    new Customer() { Id = 3, Name = "Klient C", Address = "Adres C", City = "Miasto C", Country = "Polska", PostCode = "00-000"},
+                    new Customer() { Id = 4, Name = "Klient D", Address = "Adres D", City = "Miasto D", Country = "Polska", PostCode = "00-000"},
+                    new Customer() { Id = 5, Name = "Klient E", Address = "Adres E", City = "Miasto E", Country = "Polska", PostCode = "00-000"},
+                    new Customer() { Id = 6, Name = "Klient F", Address = "Adres F", City = "Miasto F", Country = "Polska", PostCode = "00-000"},
+                    new Customer() { Id = 7, Name = "Klient G", Address = "Adres G", City = "Miasto G", Country = "Polska", PostCode = "00-000"},
+                    new Customer() { Id = 8, Name = "Klient H", Address = "Adres H", City = "Miasto H", Country = "Polska", PostCode = "00-000"}
+                }
+            };
+        }
+    }
+}
